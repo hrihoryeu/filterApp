b659e04cc1c33a24b17690bf645b55969bcf96f9
hrihoryeu <egoragrig@gmail.com>
2023-01-14 02:09:16 +0100

    hot fix

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import {AppContextProps, ContactProps} from '../../../types'
 import { AppContext } from '../../context/app.context'
 
